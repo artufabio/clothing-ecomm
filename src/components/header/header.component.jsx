f752ea32640b360ad98f02c5cf82b2ce16bbaca0
Fabio Artuso <fabioartuso88@gmail.com>
2021-05-06 13:14:52 +0100

    Added conditionals to force users to be signed in in order to navigate and buy from the shop

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,3 +1,69 @@
+// import React from 'react';
+
+// import  { Link } from 'react-router-dom';
+// import { connect } from 'react-redux';
+// import { createStructuredSelector } from 'reselect';
+
+// import CartIcon from '../cart-icon/cart-icon.components';
+// import CartDropdown from '../cart-dropdown/cart-dropdown.components';
+// import { auth } from '../../firebase/firebase.utils';
+// import { selectCartHidden } from '../../redux/cart/cart.selectors';
+// import { selectCurrentUser } from '../../redux/user/user.selectors';
+
+// import './header.styles.scss';
+// import { ReactComponent as Logo } from '../../assets/crown.svg';
+
+// const Header = ( {currentUser, hidden} ) => (
+    // <div className='header'>
+    //     <Link className='logo-container' to='/'>
+    //         <Logo className='logo'></Logo>
+    //     </Link>
+    //     <div className='options'>
+    //         <Link className='option' to='/shop'>
+    //             SHOP
+    //         </Link>
+    //         <Link className='option' to='/shop'>
+    //             CONTACT
+    //         </Link>
+    //         {
+    //             currentUser 
+    //             ?
+    //             <div className='option' onClick={ () => auth.signOut() }>SIGN OUT</div> 
+    //             :
+    //             <Link className='option' to='/signin'>SIGN IN</Link>
+    //         }
+    //         <CartIcon />
+    //     </div>
+    //     { hidden ? null : <CartDropdown /> }
+    // </div>
+// );
+
+// const mapStateToProps = ({user:{currentUser}, cart:{hidden}}) => ({
+//     currentUser,
+//     hidden
+// });
+
+// const mapStateToProps = state =>({
+//     currentUser: selectCurrentUser(state),
+//     hidden: selectCartHidden(state)
+// })
+
+// instead of the upper solutions for mapStateToProps, let's use createStructuredSelectors 
+// const mapStateToProps = createStructuredSelector({
+//     currentUser: selectCurrentUser,
+//     hidden: selectCartHidden
+// })
+
+// export default connect(mapStateToProps)(Header);
+
+
+
+// syntax used to destructor nested values
+// {user:{currentUser}, cart:{hidden}} 
+// currentUser and hidden are destructored from user and cart; user and cart are destructored off of the state
+
+// -----------------------------------------------------------------------------------------
+
 import React from 'react';
 
 import  { Link } from 'react-router-dom';
@@ -13,51 +79,49 @@ import { selectCurrentUser } from '../../redux/user/user.selectors';
 import './header.styles.scss';
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 
-const Header = ( {currentUser, hidden} ) => (
-    <div className='header'>
-        <Link className='logo-container' to='/'>
-            <Logo className='logo'></Logo>
-        </Link>
-        <div className='options'>
-            <Link className='option' to='/shop'>
-                SHOP
+const alertSignIn = () => alert('In order to navigate this website you must be signed in.')
+
+const Header = ( {currentUser, hidden} ) => {
+    if (currentUser) {
+        return (<div className='header'>
+            <Link className='logo-container' to='/'>
+                <Logo className='logo'></Logo>
             </Link>
-            <Link className='option' to='/shop'>
-                CONTACT
-            </Link>
-            {
-                currentUser 
-                ?
+            <div className='options'>
+                <Link className='option' to='/'>
+                    HOME
+                </Link>
+                <Link className='option' to='/shop'>
+                    SHOP OVERVIEW
+                </Link>
                 <div className='option' onClick={ () => auth.signOut() }>SIGN OUT</div> 
-                :
+                <CartIcon />
+            </div>
+            { hidden ? null : <CartDropdown /> }
+        </div>)
+    }
+    else {
+        return (<div className='header'>
+            <Link className='logo-container' to='/'>
+                <Logo className='logo'></Logo>
+            </Link>
+            <div className='options'>
+                <Link className='option' to='/' onClick={alertSignIn}>
+                    HOME
+                </Link>
+                <Link className='option' to='/' onClick={alertSignIn}>
+                    SHOP OVERVIEW
+                </Link>
                 <Link className='option' to='/signin'>SIGN IN</Link>
-            }
-            <CartIcon />
-        </div>
-        { hidden ? null : <CartDropdown /> }
-    </div>
-);
-
-// const mapStateToProps = ({user:{currentUser}, cart:{hidden}}) => ({
-//     currentUser,
-//     hidden
-// });
-
-// const mapStateToProps = state =>({
-//     currentUser: selectCurrentUser(state),
-//     hidden: selectCartHidden(state)
-// })
-
-// instead of the upper solutions for mapStateToProps, let's use createStructuredSelectors 
+                <CartIcon />
+            </div>
+        </div>)
+    }
+};
+ 
 const mapStateToProps = createStructuredSelector({
     currentUser: selectCurrentUser,
     hidden: selectCartHidden
 })
 
 export default connect(mapStateToProps)(Header);
-
-
-
-// syntax used to destructor nested values
-// {user:{currentUser}, cart:{hidden}} 
-// currentUser and hidden are destructored from user and cart; user and cart are destructored off of the state
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,13 +1,50 @@
+// import React from 'react';
+// import { withRouter } from 'react-router-dom';
+
+// import './menu-item.styles.scss';
+
+// const MenuItem = ({ title,imageUrl,size,history,linkUrl,match }) => {
+//     return (
+//         <div 
+//             className={`${size} menu-item`} 
+//             onClick={() => history.push(`${match.url}${linkUrl}`)}>
+//             <div 
+//                 className='background-image'
+//                 style={{ backgroundImage: `url(${imageUrl})`}} 
+//             />
+//             <div className='content'>
+//                 <h1 className='title'>{ title.toUpperCase() }</h1>
+//                 <span className='subtitle'>SHOP NOW</span>
+//             </div>
+//         </div>
+//     )
+// }
+
+// export default withRouter(MenuItem);
+
+// --------------------------------------------------------------------------------------
+
 import React from 'react';
 import { withRouter } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+
+import { selectCurrentUser } from '../../redux/user/user.selectors';
 
 import './menu-item.styles.scss';
 
-const MenuItem = ({ title,imageUrl,size,history,linkUrl,match }) => {
+const MenuItem = ({ title,imageUrl,size,history,linkUrl,match, currentUser }) => {
     return (
         <div 
             className={`${size} menu-item`} 
-            onClick={() => history.push(`${match.url}${linkUrl}`)}>
+            onClick={() => {
+                if (currentUser) {
+                    history.push(`${match.url}${linkUrl}`)
+                } else {
+                    alert('In order to navigate this website you must be signed in.')
+                    history.push(`/signin`)
+                }
+            }}>
             <div 
                 className='background-image'
                 style={{ backgroundImage: `url(${imageUrl})`}} 
@@ -20,4 +57,8 @@ const MenuItem = ({ title,imageUrl,size,history,linkUrl,match }) => {
     )
 }
 
-export default withRouter(MenuItem);
+const mapStateToProps = createStructuredSelector({
+    currentUser: selectCurrentUser
+})
+
+export default withRouter(connect(mapStateToProps)(MenuItem));
